1c3a23c122cd5f8c192eecbaa5213e3f85f71857
Burak Tungut <buraktungut@gmail.com>
2025-02-13 21:32:40 +0300

    Add deployment scripts and values.yaml for Azure DevOps agent setup

---
+++ examples/01-basic-deploy/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+### This script deploys the Azure DevOps agent to the Kubernetes cluster
+# under the `devops` namespace and
+# values from the `values.yaml` file in the same directory
+# with helm release name `azdo-agent`
+
+### PLEASE UPDATE THE VALUES.YAML FILE WITH YOUR OWN VALUES ###
+
+
+# Add the Helm repository and update it
+helm repo add btungut https://btungut.github.io
+helm repo update btungut
+
+
+# Get the directory of the script
+DEPLOY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+# Deploy the agent by using the Helm chart
+helm upgrade -i azdo-agent btungut/azure-devops-agent \
+    --namespace devops \
+    --create-namespace \
+    --values "${DEPLOY_DIR}/values.yaml"
